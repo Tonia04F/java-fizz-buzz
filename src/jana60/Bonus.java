a1591ec41d9be58c421bbad930a939c4b9391aa3
tonia <tonia@cia.como>
2022-06-09 16:43:33 +0200

    fine compito e bonus

---
+++ src/jana60/Bonus.java
@@ -0,0 +1,53 @@
+package jana60;
+
+import java.util.Scanner;
+
+public class Bonus {
+
+	public static void main(String[] args) {
+		/* Invece che definire a priori di stampare i numeri da 1 a 100, chiediamo all’utente di dirci fino a quale 
+		 * numero dobbiamo giocare, purché quel numero sia maggiore di 1 e minore di 200. 
+		 * Devo continuare a chiedere all’utente il numero fino a che non ne inserisce uno valido, 
+		 * senza interrompere l’esecuzione 
+		 * del programma.
+        */
+		
+			Scanner sc = new Scanner(System.in);
+		
+			boolean continuaAgiocare= true;
+			int numeroScelto = 0;
+			int limiteNumeri;
+		
+			while (continuaAgiocare) {
+				System.out.println("Fino a che numero dobbiamo giocare?: ");
+				limiteNumeri = sc.nextInt();
+				System.out.println("Inserisci un numero tra i e 200: ");
+				numeroScelto = sc.nextInt();
+				//controllo se il numero va bene 
+				if(numeroScelto >= 0 && numeroScelto <= 100) {
+					continuaAgiocare = false;
+					System.out.println("Hai scelto il numero: " + numeroScelto);
+				}
+				else {
+					System.out.println("Prova di nuovo!");
+				}
+				
+				
+				if(numeroScelto%3 ==0 && numeroScelto%5 ==0) {
+					System.out.println("fizzBuzz");
+				}
+				else if(numeroScelto%5 ==0) {
+					System.out.println("buzz");
+				}
+				else if(numeroScelto%3 ==0) {
+					System.out.println("Fizz");
+				}
+				
+			}
+		
+		
+		
+
+	}
+
+}
--- src/jana60/FizzBuzz.java
+++ src/jana60/FizzBuzz.java
@@ -3,8 +3,29 @@ package jana60;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*Consegna: Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi Fizz al 
+		 * posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 
+		 * 5 stampi FizzBuzz.*/
+		
+		//scrivo numeri da 1 a 100
+		for(int i = 1; i <=100; i++) {
+			System.out.println(i);
+			
+			//se sono divisibili per 3 chiedo al programma di stampare fizz
+			if(i % 5 == 0 && i % 3 == 0) {
+				System.out.println("fizzBuzz");
+			}
+			//se sono divisibili per 5 chiedo al programma di stampare buzz
+			else if(i % 5 == 0) {
+				System.out.println("buzz");
+			}
+			//se sono divisibili per 3 e per 5 chiedo al programma di stampare fizz buzz
+			else if(i % 3 == 0) {
+				System.out.println("fizz");
+			
+			}
+			
+		}
 	}
 
 }
